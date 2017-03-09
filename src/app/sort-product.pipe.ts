1c0eec20c7d715fce7291011c8e564fc0eac3af3
cvirieux <clement.virieux@gmail.com>
2017-03-09 13:06:02 +0100

    Pipe impure pour tri sur stock

--- src/app/sort-product.pipe.ts
+++ src/app/sort-product.pipe.ts
@@ -3,12 +3,12 @@ import { Product } from './model/product';
 
 
 @Pipe({
-  name: 'sortProduct'
+  name: 'sortProduct',
+  pure:false
 })
 export class SortProductPipe implements PipeTransform {
 
-  transform(value: Product[], args?: any): Product[] {
-    console.log(args);
+  transform(value: Product[], args?: any): Product[] {$
     return value.sort(function(a: Product, b: Product){
       if(a[args]<b[args]){
         return -1;
